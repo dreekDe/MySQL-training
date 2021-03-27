92fe273745c736fbc9d77e42143e70161c152a2c
dreekDe <70441036+dreekDe@users.noreply.github.com>
2021-03-27 12:45:44 +0200

    Update geography_database.sql

--- CRUD/geography_database.sql
+++ CRUD/geography_database.sql
@@ -5,6 +5,7 @@ FROM
 ORDER BY `peak_name`;
 
 
+
 SELECT 
     `country_name`, `population`
 FROM
@@ -13,3 +14,16 @@ WHERE
     `continent_code` = 'EU'
 ORDER BY `population` DESC
 LIMIT 30;
+
+
+
+SELECT 
+    `country_name`,
+    `country_code`,
+    CASE `currency_code`
+        WHEN 'EUR' THEN 'Euro'
+        ELSE 'Not Euro'
+    END AS `currency`
+FROM
+    `countries`
+ORDER BY `country_name`;
