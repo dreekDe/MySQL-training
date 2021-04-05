aaefb66bc5c356a48085ed54fa25a23255bce8d3
dreekDe <70441036+dreekDe@users.noreply.github.com>
2021-04-05 14:25:31 +0300

    Add files via upload

---
+++ DataAggregation/Ex.DataAggregation.sql
@@ -0,0 +1,14 @@
+SELECT 
+    COUNT(*)
+FROM
+    `wizzard_deposits`;
+    
+    
+    
+ SELECT 
+    MAX(magic_wand_size) AS `longest_magic_wand`
+FROM
+    `wizzard_deposits`;  
+    
+    
+    
