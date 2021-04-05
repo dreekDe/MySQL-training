37489ff9b77ded1d20a52c305ddec51be8be268a
dreekDe <70441036+dreekDe@users.noreply.github.com>
2021-04-05 14:54:59 +0300

    Update Ex.DataAggregation.sql

--- DataAggregation/Ex.DataAggregation.sql
+++ DataAggregation/Ex.DataAggregation.sql
@@ -8,7 +8,43 @@ FROM
  SELECT 
     MAX(magic_wand_size) AS `longest_magic_wand`
 FROM
-    `wizzard_deposits`;  
-    
-    
+    `wizzard_deposits`; 
+ 
+ SELECT 
+    `deposit_group`,
+    MAX(`magic_wand_size`) AS `longest_magic_wand`
+FROM
+    `wizzard_deposits`
+GROUP BY `deposit_group`
+ORDER BY `longest_magic_wand` , `deposit_group`;
+
+
+
+SELECT 
+    `deposit_group`
+FROM
+    `wizzard_deposits`
+GROUP BY `deposit_group`
+ORDER BY AVG(`magic_wand_size`)
+LIMIT 1;
+
+
+
+SELECT 
+    `deposit_group`, SUM(`deposit_amount`) AS `total_sum`
+FROM
+    `wizzard_deposits`
+GROUP BY `deposit_group`
+ORDER BY `total_sum`, `deposit_group`;  
+  
+ 
+ 
+SELECT 
+    `deposit_group`, SUM(`deposit_amount`) AS `total_sum`
+FROM
+    `wizzard_deposits`
+WHERE
+    `magic_wand_creator` = 'Ollivander Family'
+GROUP BY `deposit_group`
+ORDER BY `deposit_group`;
     
