5494a989e48263d6dcbdf946782d650a7f19c738
dreekDe <70441036+dreekDe@users.noreply.github.com>
2021-04-12 19:17:50 +0300

    Update ex-tableRelation.sql

--- TableRelations/ex-tableRelation.sql
+++ TableRelations/ex-tableRelation.sql
@@ -29,6 +29,7 @@ VALUE
 (3, 'Yana', 60200.00, 101);
 
 
+
 #2
 CREATE TABLE `manufacturers` (
   `manufacturer_id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -54,11 +55,55 @@ INSERT INTO `models`  VALUES
 (102, 'i6', 1),
 (103, 'Model S', 2),
 (104, 'Model X', 2),
-(105, 'Nova', 3);
+(105, 'Model 3', 2),
+(106, 'Nova', 3);
+
 
 
 #3
+CREATE TABLE `students` (
+    `student_id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(45) NOT NULL
+);
 
+INSERT INTO `students` VALUES
+(1, 'Mila'),                                      
+(2, 'Toni'),
+(3, 'Ron');
+
+
+CREATE TABLE `exams` (
+    `exam_id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(45) NOT NULL
+);
+
+INSERT INTO `exams` VALUES
+(101, 'Spring MVC'),
+(102, 'Neo4j'),
+(103, 'Oracle 11g');
+
+
+CREATE TABLE `students_exams` (
+    `student_id` INT,
+    `exam_id` INT,
+    CONSTRAINT pk_students_exams PRIMARY KEY (`student_id` , `exam_id`),
+    CONSTRAINT fk_students_exams_students FOREIGN KEY (`student_id`)
+        REFERENCES `students` (`student_id`),
+    CONSTRAINT fk_students_exams_exams FOREIGN KEY (`exam_id`)
+        REFERENCES `exams` (`exam_id`)
+);
+
+INSERT INTO `students_exams` VALUES
+(1,	101),
+(1,	102),
+(2,	101),
+(3,	103),
+(2,	102),
+(2,	103);
+
+
+
+#4
 
  DROP SCHEMA `now`;
 
