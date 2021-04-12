7b6cbc53d0896fcec57d708434f6661fa3b54435
dreekDe <70441036+dreekDe@users.noreply.github.com>
2021-04-12 18:35:14 +0300

    Update ex-tableRelation.sql

--- TableRelations/ex-tableRelation.sql
+++ TableRelations/ex-tableRelation.sql
@@ -1,22 +1,20 @@
 CREATE SCHEMA `now`;
 USE  `now`;
 
+#1
+CREATE TABLE `passports` (
+    `passport_id` INT PRIMARY KEY NOT NULL,
+    `passport_number` VARCHAR(30) UNIQUE 
+);
+
 CREATE TABLE `people` (
     `person_id` INT PRIMARY KEY AUTO_INCREMENT,
     `first_name` VARCHAR(50) NOT NULL,
     `salary` DECIMAL(11 , 2 ),
-    `passrport_id` INT UNIQUE
+    `passrport_id` INT UNIQUE,
+    CONSTRAINT fk_people_passport FOREIGN KEY (`passrport_id`)
+        REFERENCES `passports` (`passport_id`)
 );
-
-CREATE TABLE `passports` (
-    `passport_id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    `passport_number` VARCHAR(30) UNIQUE 
-);
-
-ALTER TABLE `people`
-ADD CONSTRAINT `fk_people_passport`
-FOREIGN KEY `people`(`passrport_id`)
-REFERENCES `passports`(`passport_id`);
 
 INSERT INTO `passports`
 VALUE 
@@ -30,7 +28,39 @@ VALUE
 (2,	'Tom', 56100.00, 103),
 (3, 'Yana', 60200.00, 101);
 
-DROP SCHEMA `now`;
+
+#2
+CREATE TABLE `manufacturers` (
+  `manufacturer_id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `established_on` DATE NOT NULL
+   );
+   
+CREATE TABLE `models` (
+    `model_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(45) NOT NULL,
+   `manufacturer_id` INT NOT NULL,
+    CONSTRAINT fk_models_manifacturers FOREIGN KEY (`manufacturer_id`)
+        REFERENCES `manufacturers` (`manufacturer_id`)
+);
+
+INSERT INTO `manufacturers`  VALUES 
+(1, 'BMW', '1916-03-01'),
+(2, 'Tesla', '2003-01-01'),
+(3, 'Lada', '1966-05-01');
+
+INSERT INTO `models`  VALUES 
+(101, 'X1', 1),
+(102, 'i6', 1),
+(103, 'Model S', 2),
+(104, 'Model X', 2),
+(105, 'Nova', 3);
+
+
+#3
+
+
+ DROP SCHEMA `now`;
 
 
 
