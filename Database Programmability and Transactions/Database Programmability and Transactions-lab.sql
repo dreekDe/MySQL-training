b776c13be9f5f778e5597d2b3afd5502cde3fc5d
dreekDe <70441036+dreekDe@users.noreply.github.com>
2021-05-03 15:18:10 +0300

    Update Database Programmability and Transactions-lab.sql

--- Database Programmability and Transactions/Database Programmability and Transactions-lab.sql
+++ Database Programmability and Transactions/Database Programmability and Transactions-lab.sql
@@ -30,30 +30,7 @@ SET
     e.`salary` = e.`salary` * 1.05
 WHERE
     d.`name` = `department_name`;
-END 
-                                                                                           
-#2nd var    
-                                                                                           
-CREATE DEFINER=`root`@`localhost` PROCEDURE `usp_raise_salaries`(`department_name` VARCHAR(45))
-BEGIN
-UPDATE `employees` AS e
-        JOIN
-    `departments` AS d ON d.department_id = e.department_id 
-SET 
-    e.`salary` = e.`salary` * 1.05
-WHERE
-    d.`name` = `department_name`;
-    
-SELECT 
-     `first_name`, 
-     `salary`
-FROM `employees` as e
-        JOIN
-    `departments` AS d ON d.department_id = e.department_id 
-WHERE  d.`name` =  `department_name`
-ORDER BY `first_name`, `salary`;
-END    
-                                                                                           
+END                                                                                     
  
 CALL usp_raise_salaries('Finance');
                                                                                            
